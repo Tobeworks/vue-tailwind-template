f28b69f6958932f8a82fc040ebc1c5249aa6ec54
Tobeworks <hallo@tobeworks.de>
2023-02-15 09:26:07 +0100

    Some modules added

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -8,6 +9,31 @@ module.exports = defineConfig({
     plugins: [
       new FaviconsWebpackPlugin(`./src/assets/logo.png`)
     ],
+    optimization: {
+      minimizer: [
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.squooshMinify,
+            options: {
+              encodeOptions: {
+                mozjpeg: {
+                  // That setting might be close to lossless, but it’s not guaranteed
+                  // https://github.com/GoogleChromeLabs/squoosh/issues/85
+                  quality: 70,
+                },
+                webp: {
+                  lossless: 1,
+                },
+                avif: {
+                  // https://github.com/GoogleChromeLabs/squoosh/blob/dev/codecs/avif/enc/README.md
+                  cqLevel: 0,
+                },
+              },
+            },
+          },
+        }),
+      ],
+    },
     module: {
       rules: [
         {
